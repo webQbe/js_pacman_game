29cea6fb8f233d8a75d315cd3f35cde02a0f898b
webQbe <webcubesolutionslk@gmail.com>
2024-11-09 19:24:13 +0530

    Defined showGameStatus() inside GameBoard class

--- GameBoard.js
+++ GameBoard.js
@@ -12,4 +12,24 @@ class GameBoard {
     /* DOMGrid represents a DOM element that acts as the main container or 
       “grid” for displaying the game’s visual elements. */
 
+    // gameWin tells whether we won the game or not
+    showGameStatus(gameWin){
+
+        // Create Element
+        const div = document.createElement('div');
+
+        // Assign Class
+        div.classList.add('game-status');
+
+        // Add Content using Turnary Operator
+        div.innerHTML = `${gameWin ? 'WIN!' : 'GAME OVER!'}`;
+
+        // Append div to DOMGrid
+        this.DOMGrid.appendChild(div);
+
+    }
+
+    /* showGameStatus() appends this new div to this.DOMGrid, 
+       so it displays on the webpage within the DOMGrid container. */
+
 }
