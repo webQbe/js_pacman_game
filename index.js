822d4297bd699eacc89ca97986176951d53c904e
webQbe <webcubesolutionslk@gmail.com>
2024-11-09 18:58:59 +0530

    Setup Initials

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ const startButton =  document.querySelector('#start-button');
 // Define Constants
 const POWER_PILL_TIME = 10000; // 10 milli-seconds
 const GLOBAL_SPEED = 80; // milli-seconds
+
+// Initial Setup
+let score = 0;
+let timer = null;
+let gameWin = flase;
+let powerPillActive = false; // Important when Pacman eats a PowerPill
+let powerPillTimer = null; // Timer for PowerPill
+
+
+
+
