5f3716c9fb458bf9150a3cef27dec3df1a4b1848
webQbe <webcubesolutionslk@gmail.com>
2024-11-09 18:51:43 +0530

    Define DOM Elements

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // Import Arrays from setup.js
 import { LEVEL, OBJECT_TYPE } from './setup';
+
+// Define DOM Elements
+const gameGrid = document.querySelector('#game');
+const scoreTable = document.querySelector('#score');
+const startButton =  document.querySelector('#start-button');
