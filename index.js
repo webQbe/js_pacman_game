0f239c2b1b8e5938645b965f79d53563ff439b00
webQbe <webcubesolutionslk@gmail.com>
2024-11-09 18:54:18 +0530

    Defined Constants

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ import { LEVEL, OBJECT_TYPE } from './setup';
 const gameGrid = document.querySelector('#game');
 const scoreTable = document.querySelector('#score');
 const startButton =  document.querySelector('#start-button');
+
+// Define Constants
+const POWER_PILL_TIME = 10000; // 10 milli-seconds
+const GLOBAL_SPEED = 80; // milli-seconds
