cd7cf6200b479fb69fbb3893cefc42d0b598296d
webQbe <webcubesolutionslk@gmail.com>
2024-11-09 19:23:15 +0530

    Created GameBoard class

---
+++ GameBoard.js
@@ -0,0 +1,15 @@
+
+// Importing From setup.js
+import { GRID_SIZE, CELL_SIZE, OBJECT_TYPE, CLASS_LIST } from "./setup";
+
+class GameBoard {
+    constructor(DOMGrid){
+        this.dotCount = 0;
+        this.grid = [];
+        this.DOMGrid = DOMGrid;
+    }
+
+    /* DOMGrid represents a DOM element that acts as the main container or 
+      “grid” for displaying the game’s visual elements. */
+
+}
